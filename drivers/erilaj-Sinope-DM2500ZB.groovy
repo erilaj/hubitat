92171e0a7ee1dbd2eeb7dd8a975ff6961e7485ef
erilaj <68398246+erilaj@users.noreply.github.com>
2020-10-26 09:21:44 -0400

    Update erilaj-Sinope-DM2500ZB.groovy

--- drivers/erilaj-Sinope-DM2500ZB.groovy
+++ drivers/erilaj-Sinope-DM2500ZB.groovy
@@ -6,15 +6,6 @@
  *  Code derived from Sinope's SmartThing dimmer DM2500ZB
  *  Source: https://raw.githubusercontent.com/Sinopetech/Smartthings/master/Sinope%20Technologies%20DM2500ZB%20V.1.0.0.txt
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
  *  
  */
 
